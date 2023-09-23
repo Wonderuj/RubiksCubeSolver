eea92461978b8260822fce0e1caa68df57baa28e
Uddhav <2020uch1479@mnit.ac.in>
2023-09-24 03:26:13 +0530

    Generic Cube

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 24-09-2023.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,14 @@
+//
+// Created by HP on 24-09-2023.
+//
+
+#ifndef RUBIKS_GENERICRUBIKSCUBE_H
+#define RUBIKS_GENERICRUBIKSCUBE_H
+
+
+class GenericRubiksCube {
+
+};
+
+
+#endif //RUBIKS_GENERICRUBIKSCUBE_H
