971d57fd823c36172bd50cfda7bd54ac2d1a4772
Uddhav <2020uch1479@mnit.ac.in>
2023-09-24 15:36:02 +0530

    Generic Cube - added comments

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "RubiksCube3d.cpp"
+int main() {
+    RubiksCube3d obj3dArray;
+
+
+//    obj3dArray.print();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+
+////    obj3dArray.l();
+////    obj3dArray.print();
+//
+//    vector<RubiksCube3d::MOVE>moveShufflelist= obj3dArray.randomShuffleCube(3);
+//    for(auto x:moveShufflelist) cout<< obj3dArray.getMove(x)<<" ";
+//    cout<<'\n';
+//
+//    obj3dArray.print();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+
+
+}
