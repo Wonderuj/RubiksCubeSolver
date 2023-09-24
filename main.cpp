dfc205cc78e9ca0e4d1e4bfd0932f6239e3cd044
Uddhav <2020uch1479@mnit.ac.in>
2023-09-24 16:19:17 +0530

    Generic Cube - added comments Rubiks cube 3d model, 1D model, and testing basic cube working in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include "RubiksCube3d.cpp"
+//#include "RubiksCube3d.cpp"
+#include "RubiksCube1d.cpp"
 int main() {
-    RubiksCube3d obj3dArray;
+
+//    --------------------------- 3D Cube testing-----------------------
+//    RubiksCube3d obj3dArray;
 
 
 //    obj3dArray.print();
 //    if(obj3dArray.isSolved()) cout<<"Solved\n";
 //    else cout<<"Not solved\n";
 
-////    obj3dArray.l();
-////    obj3dArray.print();
 //
 //    vector<RubiksCube3d::MOVE>moveShufflelist= obj3dArray.randomShuffleCube(3);
 //    for(auto x:moveShufflelist) cout<< obj3dArray.getMove(x)<<" ";
@@ -19,5 +20,32 @@ int main() {
 //    if(obj3dArray.isSolved()) cout<<"Solved\n";
 //    else cout<<"Not solved\n";
 
+//    obj3dArray.l();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+//    obj3dArray.print();
+
+//    obj3dArray.lPrime();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+//    obj3dArray.print();
+
+
+//------------------ 1D cube testing---------------------
+
+//    RubiksCube1d obj3dArray;
+//        obj3dArray.print();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+//
+//    vector<RubiksCube1d::MOVE>moveShufflelist= obj3dArray.randomShuffleCube(3);
+//    for(auto x:moveShufflelist) cout<< obj3dArray.getMove(x)<<" ";
+//    cout<<'\n';
+//
+//    obj3dArray.print();
+//    if(obj3dArray.isSolved()) cout<<"Solved\n";
+//    else cout<<"Not solved\n";
+
+// --------------------- BitBoard Cube testing-------------------
 
 }
